905e4ec09fd73b0c2d1e20357656a8726d4a27d2
Lydtk <47219609+Lydtk@users.noreply.github.com>
2020-12-20 21:53:08 +0000

    Added classification report for dummy + comments

--- Project/knn.py
+++ Project/knn.py
@@ -1,5 +1,5 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV, KFold
@@ -9,78 +9,80 @@ from sklearn.metrics import f1_score, accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix, plot_confusion_matrix
 from sklearn.metrics import roc_curve, auc
-from sklearn.preprocessing import LabelEncoder, StandardScaler
 import csv
-
 from sklearn.utils.validation import check_random_state
 
+#To find k: the optimal number of neighbours to check
 def find_k(X,y):
     kf = KFold(n_splits=5)
     f1Scores = []
-    k_range = list(range(1, 31))     #for cross-val
-    # k_range = [9]                        #after choosing best k
+    k_range = list(range(1, 50))     #for cross-val
 
-    # for k in k_range:
-    #     model =  KNeighborsClassifier(n_neighbors = k) #passing in different number of neighbours
-    #     temp = []
-    #     for train, test in kf.split(X):       #different combinations of splitting test/train according to k folds
-    #         model.fit(X[train], y[train])
-    #         ypred = model.predict(X[test])     #predicted y according to model
-    #         temp.append(f1_score(y[test], ypred, average='micro'))
+    for k in k_range:
+        model =  KNeighborsClassifier(n_neighbors = k) #passing in different number of neighbours
+        temp = []
+        for train_idx, test_idx in kf.split(X):       #different combinations of splitting test/train according to k folds
+            x_train, x_test = X.iloc[train_idx], X.iloc[test_idx]
+            y_test = y[test_idx]
+            y_train = y[train_idx]
+            model.fit(x_train, y_train)
+            ypred = model.predict(x_test)     #predicted y according to model
+            temp.append(f1_score(y_test, ypred, average='micro'))
 
-    #     f1Scores.append(np.array(temp).mean())  
+        f1Scores.append(np.array(temp).mean())#getting average f1 score of all the train test combos
 
-    knn = KNeighborsClassifier()
-    param_grid = dict(n_neighbors=k_range)
-    param_grid["weights"] = ["uniform", "distance"]
-    grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy')
-    grid.fit(X, y)
-    # Dictionary containing the parameters (k) used to generate that score
-    print(grid.best_params_)
-    print(grid.best_estimator_)
-    # print("Max Score",max(f1Scores))       # finding highest f1-best precision
-    # plt.plot(k_range, f1Scores, linewidth=2)   #score bar
-    # plt.xlabel('Number of Neighbours')
-    # plt.ylabel('F1 score')
-    # plt.title("f1 score at different K values ")
-    # plt.show()
+    #plotting f1 chart
+    plt.figure(figsize=(10,6))
+    plt.plot(k_range,f1Scores,color = 'blue',linestyle='dashed',
+            marker='o',markerfacecolor='red', markersize=10)
+    plt.title('F1 Score vs. K Value')
+    plt.xlabel('K')
+    plt.ylabel('F1 Score')
+    plt.show()
 
-def baseline(X,y):
-    dummy = DummyClassifier(strategy='uniform').fit(X, y)  #train dummy
-    ydummy = dummy.predict(X) #predict baseline
-    return ydummy
 
 def cf_matrix():
+    #split into train test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2)
-    knn = KNeighborsClassifier(n_neighbors=9)
+
+    #Predict with knn model with optimal k
+    knn = KNeighborsClassifier(n_neighbors=12)
     knn.fit(X_train, y_train)
     y_pred = knn.predict(X_test)
+
+    #Predict with dummy model
+    dummy = DummyClassifier(strategy='uniform').fit(X_train, y_train)  #train dummy
+    ydummy = dummy.predict(X_test) #predict baseline
+
+
+    #Plot Confusion Matrix -knn
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.set_title('Confusion Matrix')
+    ax.set_title('Confusion Matrix - KNN')
     plot_confusion_matrix(knn, X_test, y_test, ax = ax)
     target_names = ["Classical", "Hip-Hop", "Rock"]
+    print("Knn Classification Report")
     print(classification_report(y_test, y_pred, target_names=target_names))
     plt.show()
 
-def compare_train_test():
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=3)
-    model = KNeighborsClassifier(n_neighbors=9)
-    model.fit(X_train, y_train)
-    preds = model.predict(X_train)
-    print("TRAIN\n")
-    print(classification_report(y_train, preds))
-    print(confusion_matrix(y_train,preds))
-    
-    preds = model.predict(X_test)
-    print("TEST")
-    print(classification_report(y_test, preds))
-    print(confusion_matrix(y_test,preds))
+
+    #Plot Confusion Matrix -Dummy
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.set_title('Confusion Matrix - Dummy Classifier')
+    plot_confusion_matrix(dummy, X_test, y_test, ax = ax)
+    target_names = ["Classical", "Hip-Hop", "Rock"]
+    print("Dummy Classification Report")
+    print(classification_report(y_test, ydummy, target_names=target_names))
+    plt.show()
 
 
-def plot_multiclass_roc(n_classes, figsize=(17, 6)):
-    clf = KNeighborsClassifier(n_neighbors=9)
+def plot_multiclass_roc( n_classes):
+    #make model
+    clf = KNeighborsClassifier(n_neighbors=12)
+    #split data for training test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=3)
+    #"train" model
     clf.fit(X_train, y_train)
+    #predicted probability of 1 and -1
     y_score = clf.predict_proba(X_test)
 
     # structures
@@ -88,14 +90,15 @@ def plot_multiclass_roc(n_classes, figsize=(17, 6)):
     tpr = dict()
     roc_auc = dict()
 
-    # calculate dummies once
+    # baseline graph
     y_test_dummies = pd.get_dummies(y_test, drop_first=False).values
     for i in range(len(n_classes)):
         fpr[i], tpr[i], _ = roc_curve(y_test_dummies[:, i], y_score[:, i])
         roc_auc[i] = auc(fpr[i], tpr[i])
 
+
     # roc for each class
-    fig, ax = plt.subplots(figsize=figsize)
+    fig, ax = plt.subplots(figsize=(10,6))
     ax.plot([0, 1], [0, 1], 'k--')
     ax.set_xlim([0.0, 1.0])
     ax.set_ylim([0.0, 1.05])
@@ -109,36 +112,30 @@ def plot_multiclass_roc(n_classes, figsize=(17, 6)):
     sns.despine()
     plt.show()
 
-with open("data/train_shuffled_all.csv", "r") as f:
-    reader = csv.reader(f)
-    columns = next(reader)
-
-exclude = ["analysis_url", "id", "track_href", "type", "uri", "duration_ms", "tempo", "time_signature", "mode", "liveness", "key"]
-#exclude = ["analysis_url", "id", "track_href", "type", "uri"]
-columns_overfit = [e for e in columns if e not in exclude]
-include = ["acousticness", "energy", "instrumentalness", "loudness", "valence", "danceability", "genre"]
-
-data = pd.read_csv("data/train_shuffled_all.csv", usecols=include)
-print(data["genre"].value_counts())
-# transform categorical target variable to int
-le = LabelEncoder()
-le.fit(data.genre)
-data["genre"] = le.transform(data.genre)
-# ["hiphop", "rock", "classical"] = [1, 2, 0]
+data = pd.read_csv("data/preprocessed.csv")
+print(data.head())
+# ["classical","hiphop", "rock"] = [0,1, 2]
 X = data.iloc[:,data.columns != "genre"]  # independent feature columns
-y = data.iloc[:,-1]    # target column i.e genre
+y = data.iloc[:,-1]    #target column i.e. genre
 
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(X)
-
-scaled_df = pd.DataFrame(X, columns=X.columns)
-X = scaled_df.iloc[:,data.columns != "genre"]  # independent feature columns
+#To plot data for 2 features as an example
+# X1 = data.iloc[:,0]
+# X2 = data.iloc[:,1]
+# data_pl = {'Acousticness': X1, 'Danceability': X2, 'Genre': y}
+# sns.scatterplot(x='Acousticness', y='Danceability', hue='Genre',data=pd.DataFrame(data_pl), palette='dark')
+# plt.title("Plot of the Data")
+# plt.show()
 
 # cross val to find k -number of neighbours
 find_k(X, y)
-
-plot_multiclass_roc(n_classes=["classical", "hiphop", "rock"], figsize=(16, 10))
+# plot roc curve
+plot_multiclass_roc( n_classes=["classical", "hiphop", "rock"])
+# confusion matrices + classification reports
 cf_matrix()
-compare_train_test()
 
 
+# not used
+# def baseline(X,y):
+#     dummy = DummyClassifier(strategy='uniform').fit(X, y)  #train dummy
+#     ydummy = dummy.predict(X) #predict baseline
+#     return ydummy
